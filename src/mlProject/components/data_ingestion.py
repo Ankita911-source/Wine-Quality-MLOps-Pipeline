51a64a8e7bdd50f743eb5caed38a4879b69635a6
Ankita <tiwariankita2610@gmail.com>
2026-07-21 00:10:58 +0530

    data ingestion added

---
+++ src/mlProject/components/data_ingestion.py
@@ -0,0 +1,29 @@
+import os
+import urllib.request as request
+import zipfile
+from mlProject import logger
+from pathlib import Path
+from mlProject.utils.common import get_size
+from mlProject.entity.config_entity import DataIngestionConfig
+import ssl
+
+class data_ingestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+    def download_file(self):
+        if not os.path.exists(self.config.local_data_file):
+            ssl._create_default_https_context = ssl._create_unverified_context
+            filename, headers = request.urlretrieve(
+                url=self.config.source_URL,
+                filename=self.config.local_data_file
+            )
+            logger.info(f"{filename} downloaded with following info: \n{headers}")
+        else:
+            logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")
+
+    def extract_zip_file(self):
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
+            zip_ref.extractall(unzip_path)
